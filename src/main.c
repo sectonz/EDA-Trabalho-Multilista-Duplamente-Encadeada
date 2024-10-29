2078f9dee64e4c80a8ee0905657b49ee388c6666
sectonz <andreluizcorreiafilho@gmail.com>
2024-10-29 17:51:39 -0300

    Iniciando logica

--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
-#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "../libs/libMain.h"
 
 
 int main() {
-    char* caminho = abrirDialogoArquivo();
-
-    if (caminho != NULL) {
-        printf("Arquivo selecionado: %s\n", caminho);
-        free(caminho);
-    } else {
-        printf("Nenhum arquivo selecionado\n");
-    }
     
+    
+    //TODO
+    //leArquivo(&descritor);
+
     return 0;
 }
