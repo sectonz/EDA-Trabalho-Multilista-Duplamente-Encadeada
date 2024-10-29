69f9bea1b2fa83cc8d9e9e2dee2b1af79bdfe5c8
sectonz <andreluizcorreiafilho@gmail.com>
2024-10-29 17:04:13 -0300

    .

--- src/main.c
+++ src/main.c
@@ -1,33 +1,12 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "../libs/libMain.h"
 
-char* abrirDialogoArquivo() {
-    char* caminhoArquivo = NULL;
-
-    // Implementação GTK (Linux/Unix)
-    gtk_init(NULL, NULL);
-    
-    GtkWidget *dialog = gtk_file_chooser_dialog_new("Abrir Arquivo",
-                                                   NULL,
-                                                   GTK_FILE_CHOOSER_ACTION_OPEN,
-                                                   "_Cancelar", GTK_RESPONSE_CANCEL,
-                                                   "_Abrir", GTK_RESPONSE_ACCEPT,
-                                                   NULL);
-
-    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-        caminhoArquivo = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-    }
-    
-    gtk_widget_destroy(dialog);
-    while (gtk_events_pending()) gtk_main_iteration();
-
-    return caminhoArquivo;
-}
 
 int main() {
     char* caminho = abrirDialogoArquivo();
-    
+
     if (caminho != NULL) {
         printf("Arquivo selecionado: %s\n", caminho);
         free(caminho);
